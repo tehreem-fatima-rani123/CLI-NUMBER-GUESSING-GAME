dcb7aa6382a7c2c945a36512041b4e980907a3c2
tehreem-fatima-rani123 <saleemmsaleem651@gmail.com>
2024-05-11 02:57:43 -0700

    Adding a CLI NUMBER GUESSING GAME PROJECT

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\tWellcome To M.Saleem Cli-Number-Guessing-Game\n");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: " Enter Your guess number(Number Limit from 1 to 5): ",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congratulation ! You guess a correct number.");
+}
+else {
+    console.log("Sorry, you guess a Wrong number");
+}
